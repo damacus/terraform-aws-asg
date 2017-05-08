07335f9fb0c3dfdc928270f02b5ff2160b09b0f4
Dan Webb <dan.webb@damacus.io>
2017-05-08 08:52:43 +0100

    dd more required variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,19 @@
-variable "vpc" {}
+variable "vpc_id" {}
 variable "owner" {}
 variable "environment" {}
+variable "application" {}
 variable "name" {}
 variable "description" {
   default = "AutoScalling Group"
 }
 variable "image_id" {}
+
+# ASG Schedule
+variable "asg_min_size_up" {}
+variable "asg_max_size_up" {}
+variable "asg_desired_capacity_up" {}
+variable "schedule_recurrence_up" {}
+variable "asg_min_size_down" {}
+variable "asg_max_size_down" {}
+variable "asg_desired_capacity_down" {}
+variable "schedule_recurrence_up" {}
