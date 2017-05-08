5050f2219841d868848ce119f7837118d9fed124
Dan Webb <dan.webb@damacus.io>
2017-05-08 09:58:59 +0100

    Fix varaiables

--- variables.tf
+++ variables.tf
@@ -8,65 +8,53 @@ variable "description" {
   default = "AutoScalling Group"
 }
 
+# Security
+variable "security_groups" {
+  type = "list"
+}
+variable "instance_profile" {
+  default = ""
+}
 variable "ami" {}
-
 variable "instance_type" {
   default = "t2.medium"
 }
 
-variable "user_data_script" {}
-
 # ASG
+variable "user_data_script" {}
+variable "load_balancers" {}
+variable "zones" {}
 variable "asg_min_size" {}
-
 variable "asg_max_size" {}
-variable "asg_desired_capacity" {}
-
 variable "enabled_metrics" {
   type    = "list"
   default = ["GroupTerminatingInstances", "GroupMaxSize", "GroupDesiredCapacity", "GroupPendingInstances", "GroupInServiceInstances", "GroupMinSize", "GroupTotalInstances"]
 }
-
 variable "asg_termination_policies" {
   type        = "list"
   default     = ["OldestLaunchConfiguration", "ClosestToNextInstanceHour"]
   description = "A list of policies to decide how the instances in the auto scale group should be terminated"
 }
+variable "asg_desired_capacity" {}
 
 # ASG Schedule
+# Up
 variable "asg_min_size_up" {}
-
 variable "asg_max_size_up" {}
 variable "asg_desired_capacity_up" {}
-
 variable "schedule_recurrence_up" {
   default = "* 6 * * 1-5"
 }
-
+# Down
 variable "asg_min_size_down" {}
 variable "asg_max_size_down" {}
 variable "asg_desired_capacity_down" {}
-
-variable "asg_desired_capacity" {}
-variable "load_balancers" {}
-
 variable "schedule_recurrence_down" {
   default = "* 20 * * 1-5"
-}
-
-# Security
-variable "security_groups" {
-  type = "list"
-}
-
-variable "instance_profile" {
-  default = ""
 }
 
 # Tagging
 variable "email" {
   default = ""
 }
-
-variable "zones" {}
 variable "cost_code" {}
